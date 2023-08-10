73f686a75b9d58045ba7391f694e7e1c0047ce7c
Yasmin-Orellana <139816652+Yasmin-Orellana@users.noreply.github.com>
2023-08-10 17:39:03 +0100

    Categoria

--- SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Categoria.java
+++ SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Categoria.java
@@ -8,84 +8,74 @@ import java.util.ArrayList;
  *
  * @author MINEDUCYT
  */
-public class Categoria {
+public class Categoria 
+   {
       public int IdCategoria;
       public String Nombre;
       public String Descripcion;
       public String Imagen;
       public int top_aux;
-       public ArrayList<Producto>Producto;
-        private ArrayList<Proveedor>Proveedor; 
+      private ArrayList<Producto> producto;
 
     public Categoria() {
     }
 
-    public Categoria(int IdCategoria, String Nombre, String Descripcion, String Imagen, int top_aux, ArrayList<Producto> Producto, ArrayList<Proveedor> Proveedor) {
+    public Categoria(int IdCategoria, String Nombre, String Descripcion, String Imagen, int top_aux, ArrayList<Producto> producto) {
         this.IdCategoria = IdCategoria;
         this.Nombre = Nombre;
         this.Descripcion = Descripcion;
         this.Imagen = Imagen;
         this.top_aux = top_aux;
-        this.Producto = Producto;
-        this.Proveedor = Proveedor;
+        this.producto = producto;
     }
 
     public int getIdCategoria() {
         return IdCategoria;
     }
 
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public String getDescripcion() {
-        return Descripcion;
-    }
-
-    public String getImagen() {
-        return Imagen;
-    }
-
-    public int getTop_aux() {
-        return top_aux;
-    }
-
-    public ArrayList<Producto> getProducto() {
-        return Producto;
-    }
-
-    public ArrayList<Proveedor> getProveedor() {
-        return Proveedor;
-    }
-
     public void setIdCategoria(int IdCategoria) {
         this.IdCategoria = IdCategoria;
+    }
+
+    public String getNombre() {
+        return Nombre;
     }
 
     public void setNombre(String Nombre) {
         this.Nombre = Nombre;
     }
 
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
     public void setDescripcion(String Descripcion) {
         this.Descripcion = Descripcion;
+    }
+
+    public String getImagen() {
+        return Imagen;
     }
 
     public void setImagen(String Imagen) {
         this.Imagen = Imagen;
     }
 
+    public int getTop_aux() {
+        return top_aux;
+    }
+
     public void setTop_aux(int top_aux) {
         this.top_aux = top_aux;
     }
 
-    public void setProducto(ArrayList<Producto> Producto) {
-        this.Producto = Producto;
+    public ArrayList<Producto> getProducto() {
+        return producto;
     }
 
-    public void setProveedor(ArrayList<Proveedor> Proveedor) {
-        this.Proveedor = Proveedor;
+    public void setProducto(ArrayList<Producto> producto) {
+        this.producto = producto;
     }
-
-
-        
+    
+  
 }
